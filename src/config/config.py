e0eacdb06716a906d93b380dcb75b12cecbf90c3
dkkb <dabkb@aol.com>
2025-01-22 15:45:30 +0800

    Implement LLMProviderConfig and RouterConfig and tests.

--- src/config/config.py
+++ src/config/config.py
@@ -1,13 +1,71 @@
-from dataclasses import dataclass
-from typing import Literal
+from __future__ import annotations
+
+import json
+import logging
+from dataclasses import dataclass, asdict, field
+from typing import Literal, Union, Optional
+
+from src.load_balance.strategy import LoadBalanceStrategy
+from src.providers.base_provider import BaseLLMProvider
+from src.router.retry import RetryPolicy
+from src.utils.hash import generate_unique_id
 
 
 @dataclass
 class LogConfiguration:
+    # In dev stage, we use colored logs.
+    # In prod stage, we use json formatted logs.
     stage: Literal["dev", "prod"] = "dev"
-    verbose: bool = False
+    level: Union[logging.FATAL, logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG] = logging.DEBUG
     log_dir: str = "logs"
 
     def __post_init__(self):
         if self.stage not in ["dev", "prod"]:
             raise ValueError(f"Invalid stage value: {self.stage}")
+
+
+@dataclass
+class LLMProviderConfig:
+    model_id: str
+    impl: BaseLLMProvider
+    rpm: int
+    tpm: int
+    id: str = field(init=False)
+
+    def __post_init__(self):
+        self.id = generate_unique_id(self.to_json())
+
+    def to_json(self, indent: Optional[int] = None):
+        """
+        Convert the object to a compact JSON string ordered by keys.
+        :param indent:
+        :return:
+        """
+        return json.dumps({
+            "model_id": self.model_id,
+            "impl": self.impl.__repr__(),
+            "rpm": self.rpm,
+            "tpm": self.tpm,
+        }, default=str, indent=indent, separators=(",", ":"), sort_keys=True)
+
+
+@dataclass
+class RouterConfig:
+    # group name must be unique, so we use dict instead of list
+    llm_provider_group: dict[str, list[LLMProviderConfig]]
+
+    log_config: LogConfiguration = field(default_factory=LogConfiguration)
+    lb_strategy: LoadBalanceStrategy = LoadBalanceStrategy.CAPACITY_BASED_BALANCING
+    # The cooldown duration (in seconds) for the provider if num_fails exceeds `allowed_fails`. Default is 60.
+    cooldown_seconds: int = 60
+    num_retries: int = 3
+    retry_policy: Optional[RetryPolicy] = None
+    timeout_seconds: int = 30
+
+    def to_json(self, indent: Optional[int] = None):
+        """
+        Convert the object to a compact JSON string ordered by keys.
+        :param indent:
+        :return:
+        """
+        return json.dumps(asdict(self), default=str, indent=indent, separators=(",", ":"), sort_keys=True)
--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -1,19 +1,24 @@
+import logging
+from typing import Any
+
 import pytest
 
 from src.config import LogConfiguration
+from src.config.config import LLMProviderConfig, RouterConfig
+from src.providers.base_provider import BaseLLMProvider
 
 
 def test_default_values():
     config = LogConfiguration()
     assert config.stage == "dev"
-    assert config.verbose is False
+    assert config.level == logging.DEBUG
     assert config.log_dir == "logs"
 
 
 def test_custom_values():
-    config = LogConfiguration(stage="prod", verbose=True, log_dir="custom_logs")
+    config = LogConfiguration(stage="prod", level=logging.INFO, log_dir="custom_logs")
     assert config.stage == "prod"
-    assert config.verbose is True
+    assert config.level == logging.INFO
     assert config.log_dir == "custom_logs"
 
 
@@ -28,8 +33,90 @@ def test_invalid_stage():
         LogConfiguration(stage="invalid")
 
 
-def test_verbose():
-    config = LogConfiguration(verbose=True)
-    assert config.verbose is True
-    config = LogConfiguration(verbose=False)
-    assert config.verbose is False
+class MockLLMProvider(BaseLLMProvider):
+    async def chat_completion(self, messages: list[dict[str, str]], **kwargs) -> Any:
+        pass
+
+    async def embedding(self, texts: list[str], **kwargs) -> Any:
+        pass
+
+
+def test_provider_config_type_error():
+    gpt3_impl = MockLLMProvider()
+    with pytest.raises(TypeError):
+        LLMProviderConfig(impl=gpt3_impl, rpm=100, tpm=100)
+
+
+def test_provider_unique_id():
+    gpt3_impl = MockLLMProvider()
+    gpt3 = LLMProviderConfig(model_id="gpt3", impl=gpt3_impl, rpm=100, tpm=100)
+
+    assert gpt3.id == "f8d1b37b8f2cc0a6cdc71b3e646ec685b9b45b407cd987efbe06d0536a81b02d"
+
+
+def test_create_route_config():
+    gpt3_impl = MockLLMProvider()
+    gpt3 = LLMProviderConfig(model_id="gpt3", impl=gpt3_impl, rpm=100, tpm=100)
+    llama_impl = MockLLMProvider()
+    llama = LLMProviderConfig(model_id="llama", impl=llama_impl, rpm=100, tpm=100)
+    rc = RouterConfig(
+        llm_provider_group={"gpt3-level-model": [gpt3, llama]},
+    )
+
+    assert len(rc.llm_provider_group) == 1
+    assert len(rc.llm_provider_group["gpt3-level-model"]) == 2
+    assert rc.llm_provider_group["gpt3-level-model"][0].model_id == "gpt3"
+    assert rc.llm_provider_group["gpt3-level-model"][0].impl == gpt3_impl
+    assert rc.llm_provider_group["gpt3-level-model"][0].rpm == 100
+    assert rc.llm_provider_group["gpt3-level-model"][0].tpm == 100
+
+    assert rc.llm_provider_group["gpt3-level-model"][1].model_id == "llama"
+    assert rc.llm_provider_group["gpt3-level-model"][1].impl == llama_impl
+    assert rc.llm_provider_group["gpt3-level-model"][1].rpm == 100
+    assert rc.llm_provider_group["gpt3-level-model"][1].tpm == 100
+
+
+def test_to_json():
+    gpt3_impl = MockLLMProvider()
+    gpt3 = LLMProviderConfig(model_id="gpt3", impl=gpt3_impl, rpm=100, tpm=100)
+    llama_impl = MockLLMProvider()
+    llama = LLMProviderConfig(model_id="llama", impl=llama_impl, rpm=100, tpm=100)
+    rc = RouterConfig(
+        llm_provider_group={"gpt3-level-model": [gpt3, llama]},
+    )
+    actual = rc.to_json()
+    # print(rc.to_json(2))
+    expect = (
+        '{'
+        '  "cooldown_seconds":60,'
+        '  "lb_strategy": "LoadBalanceStrategy.CAPACITY_BASED_BALANCING",'
+
+        '  "llm_provider_group": {'
+        '    "gpt3-level-model": ['
+        '      {'
+        '        "id":"f8d1b37b8f2cc0a6cdc71b3e646ec685b9b45b407cd987efbe06d0536a81b02d",'
+        '        "impl": "<tests.config.test_config.MockLLMProvider>", '
+        '        "model_id": "gpt3", '
+        '        "rpm": 100, '
+        '        "tpm": 100'
+        '      }, '
+        '      {'
+        '        "id":"08a9167a804d07b07ad515a901a58da2b4d64a890e9db347aac981c28dfe17bc",'
+        '        "impl": "<tests.config.test_config.MockLLMProvider>", '
+        '        "model_id": "llama", '
+        '        "rpm": 100, '
+        '        "tpm": 100'
+        '      }'
+        '    ]'
+        '  }, '
+        '  "log_config": {'
+        '    "level": 10,'
+        '    "log_dir": "logs",'
+        '    "stage": "dev"'
+        '  },'
+        '  "num_retries":3,'
+        '  "retry_policy":null,'
+        '  "timeout_seconds":30'
+        '}'
+    )
+    assert actual == expect.replace(" ", "")
